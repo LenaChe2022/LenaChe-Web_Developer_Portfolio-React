0d83e4472563060b94ddebc430d65e6cddb5ee30
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-04-10 12:05:40 -0700

    added clickhandler on Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Nav() {
   const navStyle = {
@@ -7,23 +7,69 @@ export default function Nav() {
     fontWeight: 'bold',
   };
 
+  const [homestyle, setHomestyle] = useState({});
+  const [aboutmestyle, setAboutmestyle] = useState({});
+  const [contactstyle, setContactstyle] = useState({});
+  const [portfoliostyle, setPortfoliostyle] = useState({});
+  const [resumestyle, setResumestyle] = useState({});
+
+  const handleHomeClick = () => {
+    setHomestyle(navStyle);
+    setAboutmestyle({});
+    setContactstyle({});
+    setPortfoliostyle({});
+    setResumestyle({});
+  };
+
+  const handleAboutmeClick = () => {
+    setAboutmestyle(navStyle);
+    setHomestyle({});
+    setContactstyle({});
+    setPortfoliostyle({});
+    setResumestyle({});
+  };
+
+  const handlePortfolioClick = () => {
+    setPortfoliostyle(navStyle);
+    setAboutmestyle({});
+    setHomestyle({});
+    setContactstyle({});
+    setResumestyle({});
+  };
+
+  const handleContactClick = () => {
+    setContactstyle(navStyle);
+    setHomestyle({});
+    setAboutmestyle({});
+    setPortfoliostyle({});
+    setResumestyle({});
+  };
+
+  const handleResumeClick = () => {
+    setResumestyle(navStyle);
+    setHomestyle({});
+    setAboutmestyle({});
+    setPortfoliostyle({});
+    setContactstyle({});
+  };
+
   return (
     <nav className="navigation  justify-space-between-lg justify-center align-center">
         <ul>
         <li>
-          <a className="navLink" style={navStyle} href="#home">Home</a>
+          <a className="navLink" style={homestyle} href="#home" onClick={handleHomeClick}>Home</a>
         </li>
         <li>
-          <a className="navLink" href="#aboutMe">About Me</a>
+          <a className="navLink" style={aboutmestyle} href="#aboutMe" onClick={handleAboutmeClick}>About Me</a>
         </li>
         <li>
-          <a className="navLink" href="#portfolio">Portfolio</a>
+          <a className="navLink" style={portfoliostyle} href="#portfolio" onClick={handlePortfolioClick}>Portfolio</a>
         </li>
         <li>
-          <a className="navLink" href="#contact">Contact</a>
+          <a className="navLink" style={contactstyle} href="#contact" onClick={handleContactClick}>Contact</a>
         </li>
         <li>
-          <a className="navLink" href="#resume">Resume</a>
+          <a className="navLink" style={resumestyle} href="#resume" onClick={handleResumeClick}>Resume</a>
         </li>
         </ul>
     </nav>
